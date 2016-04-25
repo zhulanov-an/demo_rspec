7323f257909231a745babb6af2edd9e4167332d1
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-25 17:43:23 +0500

    add class Hero

--- hero.rb
+++ hero.rb
@@ -1,3 +1,21 @@
 class Hero
 
+  attr_reader :name
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def power_up
+    @health += 10
+  end
+
+  def power_down
+    @health -= 10
+  end
+
+  def hero_info
+    "hero #{@name} has #{@health} health"
+  end
 end
