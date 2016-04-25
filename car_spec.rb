c3b456c086b416744cf45e59911376b50ec41f3c
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-25 19:14:21 +0500

    add 3 test to car

--- car_spec.rb
+++ car_spec.rb
@@ -1,5 +1,22 @@
 require './car'
 
 describe Car do
-  
+  before do
+    @car = Car.new
+  end
+
+  it 'fuel is zero' do
+    expect(@car.fuel).to eq 0
+  end
+
+  it 'check constant MILES_PER_GALLON' do
+    expect(Car::MILES_PER_GALLON).to eq 20
+  end
+
+  it 'add fuel amount' do
+    @car.add_fuel(10)
+    expect(@car.fuel).to eq 10
+  end
+
+
 end
