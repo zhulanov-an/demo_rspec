c1350e6f05978a89e1f006a3c9b077450204e378
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-25 18:24:36 +0500

    remove DRY Hero.new

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,37 +1,34 @@
 require './hero'
 
 describe Hero do
+  before do
+    @hero = Hero.new 'mike'
+  end
 
   it "has a capitalized name" do
-    hero = Hero.new 'mike'
-    expect(hero.name).to eq 'Mike'
+    expect(@hero.name).to eq 'Mike'
   end
 
   it "it can power up" do
-    hero = Hero.new 'mike'
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "it can power down" do
-    hero = Hero.new 'mike'
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
 
   it "displays full hero info" do
-    hero = Hero.new 'mike'
-    expect(hero.info).to eq 'Hero Mike has a health of 100'
+    expect(@hero.info).to eq 'Hero Mike has a health of 100'
   end
 
   it "displays info hero after power up" do
-    hero = Hero.new 'mike'
-    hero.power_up
-    expect(hero.info).to eq 'Hero Mike has a health of 110'
+    @hero.power_up
+    expect(@hero.info).to eq 'Hero Mike has a health of 110'
   end
 
   it "displays info hero after power down" do
-    hero = Hero.new 'mike'
-    hero.power_down
-    expect(hero.info).to eq 'Hero Mike has a health of 90'
+    @hero.power_down
+    expect(@hero.info).to eq 'Hero Mike has a health of 90'
   end
 
 end
