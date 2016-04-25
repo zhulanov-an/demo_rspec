41f60e1be5e04b575f6e94bace2f473096915227
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-25 18:00:02 +0500

    add test Hero def power_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -6,5 +6,9 @@ describe Hero do
     hero = Hero.new 'mike'
     expect(hero.name).to eq 'Mike'
   end
- 
+
+  it "it can power up can do" do
+    hero = Hero.new 'mike'
+    expect(hero.power_up).to eq 110
+  end
 end
